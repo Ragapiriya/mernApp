32f51a7c789b0d9e6fbfeeb4bff3dbca3aa1a859
Ragapiriya <r2001piriya@gmail.com>
2024-06-27 21:40:10 +0530

    MongoDB connection review

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from "url";
 const __filename= fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename);
 dotenv.config();
-const app= express();
+const app= express();  
 
 app.use(express.json());
 app.use(helmet());
@@ -36,8 +36,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage }); 
  
 //Mongoose setup
-
-const PORT = process.env.PORT || 8081; //if 9000 port doesn't work, 9001 will.
+ 
+const PORT = process.env.PORT || 8081; //if 8080 port doesn't work, 9001 will.
 console.log("MongoDB URI: ",process.env.MONGO_URL); 
 mongoose
   .connect(process.env.MONGO_URL) 
